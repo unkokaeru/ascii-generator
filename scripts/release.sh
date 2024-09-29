5d18691d39ca2687b5e2a32afd7533bda61baead
William Fayers <william@fayers.com>
2024-09-29 16:38:03 +0100

    fix: release flow

--- scripts/release.sh
+++ scripts/release.sh
@@ -79,18 +79,17 @@ git add .
 git commit -m "chore: bump version from $current_version to $new_version"
 echo "Version bump committed successfully."
 
-# Create a new tag and push it to the remote repository
-echo "Creating a new tag for the release..."
-git tag -a "v$new_version" -m "Release $new_version"
-
-git config pull.rebase true  # Ensure that the tag is pushed correctly
-
+# Pull the latest changes from the remote repository
+git config pull.rebase true  # Ensure that the pull is a rebase
 echo "Pulling the latest changes from the remote repository..."
 if ! git pull origin main; then
     echo "Failed to pull the latest changes from the remote repository."
     exit 5
 fi
 
+# Create a new tag and push it to the remote repository
+echo "Creating a new tag for the release..."
+git tag -a "v$new_version" -m "Release $new_version"
 echo "Pushing the new tag to the remote repository..."
 git push --follow-tags
 echo "Release $new_version created successfully."
